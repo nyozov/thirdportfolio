6502cec6c0e7b718c2f68b4e0736dc6dc2aff032
nikolay <n_yozov@hotmail.com>
2022-04-28 17:25:30 -0400

    img zoom hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,37 +12,31 @@ const Projects = () => (
       <div className="md:mt-24 f-f-p">
         <div className=" md:flex justify-center w-full">
           <div className="flex flex-col items-center md:items-end md:pr-12 md:border-r-4 border-gray-300 relative md:w-1/2">
-            <ParallaxLayer
-            factor={1}
-            offset={1.9}
-            speed={2}
             
-            >
-            <motion.div whileHover={{scale:1.1}} aria-label="sign up" role="img">
-              <img className="focus:outline-none mr-2 mt-10 shadow cursor-pointer" src={myProjects[0].img} alt="how it work" />
+            <motion.div className="img-hover-zoom"  aria-label="sign up" role="img">
+              <img className=" focus:outline-none mr-2 mt-10 shadow cursor-pointer" src={myProjects[0].img} alt="how it work" />
             </motion.div>
 
-            </ParallaxLayer>
-            
-            <div className="flex mt-56 flex-col items-center lg:items-end md:w-8/12">
+        
+            <div className="flex mt-12 flex-col items-center lg:items-end md:w-8/12">
               <h1 className="focus:outline-none text-xl font-bold leading-5 text-white">E-Tune</h1>
-              <h2 className="focus:outline-none text-gray-500 mt-3 pl-3 text-center md:text-right text-base leading-6 tracking-wide">Create a wallet by linking a payment method that you’re comfortable with. Paypal, Visa and more </h2>
+              <h2 className="focus:outline-none text-gray-500 mt-3 pl-3 text-center md:text-right text-base leading-6 tracking-wide">{myProjects[1].desc} </h2>
             </div>
             <div aria-label="transactions" role="img">
               <img className="focus:outline-none mt-24" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg3.svg" alt />
             </div>
             <img className="hidden md:block absolute right-0 top-0 -mt-2 -mr-1" src="https://cdn.tuk.dev/assets/components/111220/Fs7/line.png" alt />
-          </div>
+          </div> 
           <div className="flex flex-col items-center md:items-start md:pl-12 lg:border-gray-400 mt-20 md:mt-0 md:w-1/2">
             
             
             <div className="flex mt-6 flex-col items-center md:items-start md:w-8/12">
               <h1 className="focus:outline-none text-xl font-bold leading-5 text-white">Gallery</h1>
-              <h2 className="focus:outline-none text-gray-500 mt-3 text-base leading-6 tracking-wide">Sign up on our website and opt for a program that suits you the best . </h2>
+              <h2 className="focus:outline-none text-gray-500 mt-3 text-base leading-6 tracking-wide">{myProjects[0].desc} </h2>
             </div>
-            <div aria-label="wallet" role="img">
+            <motion.div className="img-hover-zoom cursor-pointer" aria-label="wallet" role="img">
               <img className="focus:outline-none mt-32" src={myProjects[1].img} alt />
-            </div>
+            </motion.div>
             
             <div className="flex mt-6 flex-col items-center md:items-start md:w-8/12">
               <h1 className="focus:outline-none text-xl font-bold leading-5">Start your Transactions</h1>
