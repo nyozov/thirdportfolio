ba77170461f91b6272d93375323936ede0ef3b92
nikolay <n_yozov@hotmail.com>
2022-05-13 14:40:14 -0400

    change scroll indicator

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function ScrollIndicator({parallax}) {
+export default function ScrollIndicator({ parallax }) {
 
-  const [page, setPage] = useState(0)
   return (
-    <div className="z-10 text-white absolute left-4 h-screen flex justify-center items-center flex-col">
+    <div className="z-10 text-shadow text-white absolute left-4 h-screen flex justify-center items-center flex-col">
+     <div>
+       ^
+     </div>
       <div
-            onClick={() => parallax.current.scrollTo(0)}
-       className="border border-white w-3 h-3 rounded-full hover:bg-gray-300 cursor-pointer"></div>
-      <div 
-             onClick={() => parallax.current.scrollTo(1)}
-      className="mt-2 border border-white w-3 h-3 rounded-full hover:bg-gray-300 cursor-pointer"
-      
+        onClick={() => parallax.current.scrollTo(0)}
+        className="border border-white w-[10px] h-[10px] rounded-full hover:bg-gray-300 cursor-pointer"
+      ></div>
+      <div
+        onClick={() => parallax.current.scrollTo(1)}
+        className="mt-[5px] border border-white w-[10px] h-[10px] rounded-full hover:bg-gray-300 cursor-pointer"
       />
       <div
         onClick={() => parallax.current.scrollTo(2)}
-       className="mt-2 border border-white w-3 h-3 rounded-full hover:bg-gray-300 cursor-pointer"></div>
-      <div 
-      onClick={() => parallax.current.scrollTo(3)}
-      className="mt-2 border border-white w-3 h-3 rounded-full hover:bg-gray-300 cursor-pointer"></div>
+        className="mt-[5px] border border-white w-[10px] h-[10px] rounded-full hover:bg-gray-300 cursor-pointer"
+      ></div>
+      <div
+        onClick={() => parallax.current.scrollTo(3)}
+        className="mt-[5px] border border-white w-[10px] h-[10px] rounded-full hover:bg-gray-300 cursor-pointer"
+      ></div>
+        <div>
+       ⌄
+     </div>
     </div>
   );
 }
