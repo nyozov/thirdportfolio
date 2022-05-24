1c1eb0787806ba88a55a1f62c76c071bf5a3aac7
nikolay <n_yozov@hotmail.com>
2022-05-24 16:01:43 -0400

    add email loading, notifaction delay

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,9 +43,8 @@ export default function Contact() {
       })
       .then((res) => {
         if (res.data.success) {
-          setLoading(false);
-          setMode("success");
-          setFlag(true);
+          setTimeout(() => setLoading(false), 1000);
+          setTimeout(() => setFlag(true), 1000);
         } else {
           setMode("error");
         }
@@ -59,94 +58,94 @@ export default function Contact() {
 
   return (
     <div>
-     {!flag && 
-    <div className="w-screen flex flex-col justify-center items-center">
-      <h1 className="focus:outline-none text-center text-3xl lg:text-4xl font-extrabold lg:leading-9 tracking-wider text-shadow text-gray-200">
-        Contact
-      </h1>
-      <p className="text-gray-400 text-sm text-left p-2">
-        Send me a message here or contact me though LinkedIn/Github
-      </p>
-      <form className="w-full max-w-lg mt-6">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-200 text-xs font-bold mb-2"
-              for="grid-password"
-            >
-              E-mail
-            </label>
-            <input
-              className="appearance-none block w-full bg-transparent text-gray-300 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500"
-              onChange={handleChange}
-              type="text"
-              id="name"
-              name="name"
-              maxLength="50"
-              required
-              placeholder="Your email/name"
-            />
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-200 text-xs font-bold mb-2"
-              for="grid-password"
-            >
-              Message
-            </label>
-            <textarea
-              className=" no-resize appearance-none block w-full bg-transparent text-gray-300 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500 h-48 resize-none"
-              onChange={handleChange}
-              maxLength="200"
-              id="message"
-              name="message"
-              required
-              placeholder="Your message"
-            />
-            <div
-              className={
-                !charNumber
-                  ? `text-red-700 text-sm text-right px-2 mt-[-40px]`
-                  : `text-sm text-gray-200 text-right px-2 mt-[-40px]`
-              }
-            >
-              {charNumber}
+      {!flag && (
+        <div className="w-screen flex flex-col justify-center items-center">
+          <h1 className="focus:outline-none text-center text-3xl lg:text-4xl font-extrabold lg:leading-9 tracking-wider text-shadow text-gray-200">
+            Contact
+          </h1>
+          <p className="text-gray-400 text-sm text-left p-2">
+            Send me a message here or contact me though LinkedIn/Github
+          </p>
+          <form onSubmit={handleSubmit} className="w-full max-w-lg mt-6">
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-200 text-xs font-bold mb-2"
+                  for="grid-password"
+                >
+                  E-mail
+                </label>
+                <input
+                  className="appearance-none block w-full bg-transparent text-gray-300 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500"
+                  onChange={handleChange}
+                  type="text"
+                  id="name"
+                  name="name"
+                  maxLength="50"
+                  required
+                  placeholder="Your email/name"
+                />
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center w-full">
-          <button
-            onClick={() => setFlag(true)}
-            type="submit"
-            className="clicker border border-white font-semibold text-white p-3 hover:bg-gray-500 shadow-md flex bg-div justify-center w-36 items-center  border-gray-200"
-          >
-            {!loading && <p>Send</p>}
-            {loading && (
-              <svg
-                role="status"
-                class="w-[23.5px] h-[23.5px] mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-200 text-xs font-bold mb-2"
+                  for="grid-password"
+                >
+                  Message
+                </label>
+                <textarea
+                  className=" no-resize appearance-none block w-full bg-transparent text-gray-300 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500 h-48 resize-none"
+                  onChange={handleChange}
+                  maxLength="200"
+                  id="message"
+                  name="message"
+                  required
+                  placeholder="Your message"
+                />
+                <div
+                  className={
+                    !charNumber
+                      ? `text-red-700 text-sm text-right px-2 mt-[-40px]`
+                      : `text-sm text-gray-200 text-right px-2 mt-[-40px]`
+                  }
+                >
+                  {charNumber}
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center items-center w-full">
+              <button
+               
+                type="submit"
+                className="clicker border border-white font-semibold text-white p-3 hover:bg-gray-500 shadow-md flex bg-div justify-center w-36 items-center  border-gray-200"
               >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-            )}
-          </button>
+                {!loading && <p>Send</p>}
+                {loading && (
+                  <svg
+                    role="status"
+                    class="w-[23.5px] h-[23.5px] mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-     }
-    <Notification flag={flag} setFlag={setFlag}/>
+      )}
+      <Notification flag={flag} setFlag={setFlag} />
     </div>
   );
 }
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -10,18 +10,15 @@ const Index = ({flag, setFlag}) => {
                             <path className="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z" />
                         </svg>
                     </div>
-                    <div className="flex flex-col justify-center xl:-ml-4 pl-4 xl:pl-1 sm:w-3/5 pt-4 sm:pb-4 pb-2">
-                        <p className="text-lg text-gray-800 dark:text-gray-100 font-semibold pb-1">Message Sent</p>
-                        <p className="text-sm text-gray-600 dark:text-gray-400 font-normal">You have successfully sent an email. Thank you for the message/feedback.</p>
-                    </div>
-                    <div className="flex sm:flex-col sm:justify-center sm:border-l dark:border-gray-700 items-center border-gray-300 sm:w-1/6 pl-4 sm:pl-0">
-                        <div className="sm:pt-4 pb-4 sm:border-b dark:border-gray-700 border-gray-300 sm:w-full flex sm:justify-center">
-                            <span className="sm:text-sm text-xs text-green-400 font-bold mr-4 sm:mr-0 cursor-pointer">View</span>
-                        </div>
-                        <div className="sm:pt-4 pb-4 flex sm:justify-center w-full cursor-pointer" onClick={()=> setFlag(false)}>
+                   
+                        <p className="p-2 text-lg text-gray-800 dark:text-gray-100 font-semibold pb-1">Message Sent</p>
+                        <p className="p-2 text-sm text-gray-600 dark:text-gray-400 font-normal">You have successfully sent an email. Thank you for the message/feedback.</p>
+                    
+                      
+                        <div className="hover:bg-gray-700 sm:pt-4 pb-4 flex sm:justify-center w-full cursor-pointer" onClick={()=> setFlag(false)}>
                             <span className="sm:text-sm text-xs text-gray-600 dark:text-gray-400 cursor-pointer">Dismiss</span>
                         </div>
-                    </div>
+                    
                 </div>
             </div>
 
