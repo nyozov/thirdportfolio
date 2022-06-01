30caa1cfcb7ca3e7a769495c5d0034ae73894049
nikolay <n_yozov@hotmail.com>
2022-06-01 12:43:23 -0400

    fix nav dropdown changing title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { HiLocationMarker } from "react-icons/hi";
 export default function Title() {
   return (
@@ -6,8 +5,7 @@ export default function Title() {
       <div className="flex w-screen flex-col h-screen items-center justify-center ">
         <div className="flex z-index-behind flex-col h-full justify-center items-center absolute top-0">
           <h1 className="text-white text-shadow font-bold text-3xl">
-            <span className=''>F</span>ull Stack Web
-            Developer
+            <span className="">F</span>ull Stack Web Developer
           </h1>
           <span className="text-white">/</span>
           <h1 className="text-white text-shadow text-2xl">UI/UX Designer</h1>
@@ -15,16 +13,13 @@ export default function Title() {
             <HiLocationMarker className="w-8" />
             <h1 className="ml-[-5px]">Toronto</h1>
           </div>
-          
         </div>
-        <a
-            href="https://resume.creddle.io/resume/dykdi4p41za"
-            target="_blank"
-          >
-            <div className="white-glassmorphism shadow-md text-white clicker p-2 px-4 mt-4  hover:border-white">
-              My Resume
-            </div>
-          </a>
+        <a href="https://resume.creddle.io/resume/dykdi4p41za" target="_blank">
+          {" "}
+          <div className="absolute top-2/3 mt-[-40px] left-1/2 transform -translate-x-1/2 -translate-y-2/3 white-glassmorphism shadow-md text-white clicker p-2 px-4 mt-6  hover:border-white">
+            My Resume
+          </div>
+        </a>
       </div>
     </div>
   );
